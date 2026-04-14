e1302faea6d79f4c22f8e77ed5225cfe88141a7d
franco <moscatofranco04@gmail.com>
2026-04-13 22:07:04 -0300

    recuperando version util

--- dags/dag_manual_migration.py
+++ dags/dag_manual_migration.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 @dag(
     dag_id="model_manual_migration",
-    description="Migra un modelo seleccionado de manera manual usando solo run_id",
+    description="Migra un modelo seleccionado de manera manual usando model_id",
     start_date=datetime(2024, 1, 1),
     params={
-        "run_id": "COMPLETAR",
+        "model_id": "COMPLETAR",
         "registered_model_name": "gas_model",
     },
 )
@@ -22,30 +22,28 @@ def model_manual_migration():
         context = get_current_context()
         params = context["params"]
 
-        run_id = params["run_id"]
+        model_id = params["model_id"]
         model_name = params["registered_model_name"]
 
-        if not run_id:
-            raise ValueError("Debes especificar run_id")
+        if not model_id or model_id == "COMPLETAR":
+            raise ValueError("Debes especificar model_id")
 
         client = MlflowClient()
+        model_uri = f"models:/{model_id}"
 
-        model_uri = f"runs:/{run_id}/model"
-
-        # Crear modelo si no existe
+        # Crear modelo registrado si no existe
         try:
             client.create_registered_model(model_name)
         except Exception:
             pass
 
-        # nueva version
+        # Nueva versión del modelo
         mv = client.create_model_version(
             name=model_name,
             source=model_uri,
-            run_id=run_id,
         )
 
-        # migramos
+        # Promover a production (alias)
         client.set_registered_model_alias(
             name=model_name,
             alias="production",
